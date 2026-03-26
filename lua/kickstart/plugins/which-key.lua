af20e2ae5e25158e9544f2efef9014ba90923cc1
Shane DuPrey <36708583+Fepozopo@users.noreply.github.com>
2026-03-25 21:15:33 -0700

    Add language servers, formatters, and UI tweaks
    Enable additional LSP servers and installer tools (clangd, gopls,
    rust_analyzer, goimports, ruff, etc.). Expand Treesitter parsers for
    more languages. Configure Neo-tree to show hidden files, disable netrw
    hijack, and not open at startup. Add new which-key groups and tidy a
    few mappings.

--- lua/kickstart/plugins/which-key.lua
+++ lua/kickstart/plugins/which-key.lua
@@ -28,10 +28,14 @@ return {
 
       -- Document existing key chains
       spec = {
-        { '<leader>s', group = '[S]earch', mode = { 'n', 'v' } },
-        { '<leader>t', group = '[T]oggle' },
-        { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } }, -- Enable gitsigns recommended keymaps first
-        { 'gr', group = 'LSP Actions', mode = { 'n' } },
+        { '<leader>s',  group = '[S]earch',    mode = { 'n', 'v' } },
+        { '<leader>t',  group = '[T]oggle' },
+        { '<leader>h',  group = 'Git [H]unk',  mode = { 'n', 'v' } }, -- Enable gitsigns recommended keymaps first
+        { 'gr',         group = 'LSP Actions', mode = { 'n' } },
+        { '<leader>a',  group = '[A]I' },
+        { '<leader>m',  group = '[M]ake' },
+        { '<leader>ms', group = '[S]plit' },
+        { '<leader>mt', group = '[T]erminal' },
       },
     },
   },
