da72c37c06501001836be0f23b65c1c46ea8a97a
Shane DuPrey <36708583+Fepozopo@users.noreply.github.com>
2026-04-11 16:47:45 -0700

    Adjust keymaps and which-key groups
    Use <C-S-Left/Right/Down/Up> for window moves to improve terminal
    compatibility. Remove commented Pi AI keymaps. Add default
    mode = { 'n', 'v' } to which-key spec and drop the <leader>mt terminal
    group.

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -47,10 +47,10 @@ vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower win
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
-vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
-vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
-vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
-vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
+vim.keymap.set("n", "<C-S-Left>", "<C-w>H", { desc = "Move window to the left" })
+vim.keymap.set("n", "<C-S-Right>", "<C-w>L", { desc = "Move window to the right" })
+vim.keymap.set("n", "<C-S-Down>", "<C-w>J", { desc = "Move window to the lower" })
+vim.keymap.set("n", "<C-S-Up>", "<C-w>K", { desc = "Move window to the upper" })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
@@ -112,12 +112,6 @@ vim.keymap.set('n', '<leader>tm', ':MarkdownPreviewToggle<CR>', { desc = '[T]ogg
 
 -- Oil file explorer
 vim.keymap.set('n', '<leader>e', ':Oil --float<CR>', { desc = '[E]xplore files' })
-
--- -- pi AI keymaps
--- -- Ask pi with the current buffer as context
--- vim.keymap.set("n", "<leader>ai", ":PiAsk<CR>", { desc = "Ask pi" })
--- -- Ask pi with visual selection as context
--- vim.keymap.set("v", "<leader>ai", ":PiAskSelection<CR>", { desc = "Ask pi (selection)" })
 
 -- Copilot AI keymaps
 vim.keymap.set('n', '<leader>ct', ':Copilot toggle<CR>', { desc = '[A]I Copilot [T]oggle Buffer Status' })
