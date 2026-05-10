ffc4fd8e6cb35db06e744e3b8c344c9dfbfacfa0
Shane DuPrey <36708583+Fepozopo@users.noreply.github.com>
2026-05-09 19:54:57 -0700

    Add keymaps and insert-line commands
    Add various keybindings: Copilot controls, MarkdownPreview toggle,
    Oil file explorer, window resize and movement, line move/indent/join
    shortcuts, and mappings to insert indented lines above or below
    without entering insert mode.

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -8,17 +8,17 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Diagnostic Config & Keymaps
 -- See :help vim.diagnostic.Opts
 vim.diagnostic.config {
-    update_in_insert = false,
-    severity_sort = true,
-    float = { border = 'rounded', source = 'if_many' },
-    underline = { severity = { min = vim.diagnostic.severity.WARN } },
+  update_in_insert = false,
+  severity_sort = true,
+  float = { border = 'rounded', source = 'if_many' },
+  underline = { severity = { min = vim.diagnostic.severity.WARN } },
 
-    -- Can switch between these as you prefer
-    virtual_text = true,   -- Text shows up at the end of the line
-    virtual_lines = false, -- Text shows up underneath the line, with virtual lines
+  -- Can switch between these as you prefer
+  virtual_text = true, -- Text shows up at the end of the line
+  virtual_lines = false, -- Text shows up underneath the line, with virtual lines
 
-    -- Auto open the float, so you can easily read the errors when jumping with `[d` and `]d`
-    jump = { float = true },
+  -- Auto open the float, so you can easily read the errors when jumping with `[d` and `]d`
+  jump = { float = true },
 }
 
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
@@ -47,10 +47,10 @@ vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower win
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
-vim.keymap.set("n", "<C-S-Left>", "<C-w>H", { desc = "Move window to the left" })
-vim.keymap.set("n", "<C-S-Right>", "<C-w>L", { desc = "Move window to the right" })
-vim.keymap.set("n", "<C-S-Down>", "<C-w>J", { desc = "Move window to the lower" })
-vim.keymap.set("n", "<C-S-Up>", "<C-w>K", { desc = "Move window to the upper" })
+vim.keymap.set('n', '<C-S-Left>', '<C-w>H', { desc = 'Move window to the left' })
+vim.keymap.set('n', '<C-S-Right>', '<C-w>L', { desc = 'Move window to the right' })
+vim.keymap.set('n', '<C-S-Down>', '<C-w>J', { desc = 'Move window to the lower' })
+vim.keymap.set('n', '<C-S-Up>', '<C-w>K', { desc = 'Move window to the upper' })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
@@ -59,55 +59,21 @@ vim.keymap.set("n", "<C-S-Up>", "<C-w>K", { desc = "Move window to the upper" })
 --  Try it with `yap` in normal mode
 --  See `:help vim.hl.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
-    desc = 'Highlight when yanking (copying) text',
-    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-    callback = function() vim.hl.on_yank() end,
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function() vim.hl.on_yank() end,
 })
 
 -- vim: ts=2 sts=2 sw=2 et
 
--- better movement in wrapped text
-vim.keymap.set('n', 'j', function()
-    return vim.v.count == 0 and 'gj' or 'j'
-end, { expr = true, silent = true, desc = 'Down (wrap-aware)' })
-vim.keymap.set('n', 'k', function()
-    return vim.v.count == 0 and 'gk' or 'k'
-end, { expr = true, silent = true, desc = 'Up (wrap-aware)' })
-
-vim.keymap.set('n', '<C-Up>', '<C-w>+', { desc = 'Increase window height' })
-vim.keymap.set('n', '<C-Down>', '<C-w>-', { desc = 'Decrease window height' })
-vim.keymap.set('n', '<C-Left>', '<C-w><', { desc = 'Decrease window width' })
-vim.keymap.set('n', '<C-Right>', '<C-w>>', { desc = 'Increase window width' })
-
-vim.keymap.set('n', '<A-j>', ':m .+1<CR>==', { desc = 'Move line down' })
-vim.keymap.set('n', '<A-k>', ':m .-2<CR>==', { desc = 'Move line up' })
-vim.keymap.set('v', '<A-j>', ":m '>+1<CR>gv=gv", { desc = 'Move selection down' })
-vim.keymap.set('v', '<A-k>', ":m '<-2<CR>gv=gv", { desc = 'Move selection up' })
-
-vim.keymap.set('v', '<', '<gv', { desc = 'Indent left and reselect' })
-vim.keymap.set('v', '>', '>gv', { desc = 'Indent right and reselect' })
-
-vim.keymap.set('n', 'J', 'mzJ`z', { desc = 'Join lines and keep cursor position' })
-
-vim.keymap.set({ 'n' }, '<D-s>', ':w<CR>', { desc = 'Save file' })
-vim.keymap.set({ 'i' }, '<D-s>', '<ESC>:w<CR>a', { desc = 'Save file' })
-
-vim.keymap.set('n', '<C-n>', ':tabe<CR>', { desc = 'Create new tab' })
-
--- ================= Make keymaps =================
--- Make terminal splits
-vim.api.nvim_set_keymap('n', '<leader>mtv', ':vsplit | terminal<CR>',
-    { noremap = true, silent = true, desc = '[M]ake [T]erminal ([V]ertical Split)' })
-vim.api.nvim_set_keymap('n', '<leader>mth', ':split | terminal<CR>',
-    { noremap = true, silent = true, desc = '[M]ake [T]erminal ([H]orizontal Split)' })
--- Make splits
-vim.api.nvim_set_keymap('n', '<leader>msv', ':vsplit<CR>',
-    { noremap = true, silent = true, desc = '[M]ake [S]plit [V]ertical' })
-vim.api.nvim_set_keymap('n', '<leader>msh', ':split<CR>',
-    { noremap = true, silent = true, desc = '[M]ake [S]plit [H]orizontal' })
--- Make tabs
-vim.api.nvim_set_keymap('n', '<leader>mn', ':tabnew<CR>:Oil<CR>',
-    { noremap = true, silent = true, desc = '[M]ake [N]ew Tab' })
+-- Copilot AI keymaps
+vim.keymap.set('n', '<leader>ct', ':Copilot toggle<CR>', { desc = '[C]opilot [T]oggle Buffer Status' })
+vim.keymap.set('n', '<leader>cs', ':Copilot status<CR>', { desc = '[C]opilot [S]tatus' })
+vim.keymap.set('n', '<leader>cl', ':Copilot model list<CR>', { desc = '[C]opilot [L]ist Models' })
+vim.keymap.set('n', '<leader>cp', ':Copilot panel<CR>', { desc = '[C]opilot [P]anel' })
+vim.keymap.set('n', '<leader>ca', ':Copilot suggestion toggle_auto_trigger<CR>', { desc = '[C]opilot Toggle [A]uto Trigger' })
+vim.keymap.set('n', '<leader>cd', ':Copilot disable<CR>', { desc = '[C]opilot [D]isable' })
+vim.keymap.set('n', '<leader>ce', ':Copilot enable<CR>', { desc = '[C]opilot [E]nable' })
 
 -- Toggle Markdown preview
 vim.keymap.set('n', '<leader>tm', ':MarkdownPreviewToggle<CR>', { desc = '[T]oggle [M]arkdown Preview' })
@@ -115,12 +81,69 @@ vim.keymap.set('n', '<leader>tm', ':MarkdownPreviewToggle<CR>', { desc = '[T]ogg
 -- Oil file explorer
 vim.keymap.set('n', '<leader>e', ':Oil --float<CR>', { desc = '[E]xplore files' })
 
--- Copilot AI keymaps
-vim.keymap.set('n', '<leader>ct', ':Copilot toggle<CR>', { desc = '[C]opilot [T]oggle Buffer Status' })
-vim.keymap.set('n', '<leader>cs', ':Copilot status<CR>', { desc = '[C]opilot [S]tatus' })
-vim.keymap.set('n', '<leader>cl', ':Copilot model list<CR>', { desc = '[C]opilot [L]ist Models' })
-vim.keymap.set('n', '<leader>cp', ':Copilot panel<CR>', { desc = '[C]opilot [P]anel' })
-vim.keymap.set('n', '<leader>ca', ':Copilot suggestion toggle_auto_trigger<CR>',
-    { desc = '[C]opilot Toggle [A]uto Trigger' })
-vim.keymap.set('n', '<leader>cd', ':Copilot disable<CR>', { desc = '[C]opilot [D]isable' })
-vim.keymap.set('n', '<leader>ce', ':Copilot enable<CR>', { desc = '[C]opilot [E]nable' })
+-- better movement in wrapped text
+vim.keymap.set('n', 'j', function() return vim.v.count == 0 and 'gj' or 'j' end, { expr = true, silent = true, desc = 'Down (wrap-aware)' })
+vim.keymap.set('n', 'k', function() return vim.v.count == 0 and 'gk' or 'k' end, { expr = true, silent = true, desc = 'Up (wrap-aware)' })
+
+-- Change window size
+vim.keymap.set('n', '<C-Up>', '<C-w>+', { desc = 'Increase window height' })
+vim.keymap.set('n', '<C-Down>', '<C-w>-', { desc = 'Decrease window height' })
+vim.keymap.set('n', '<C-Left>', '<C-w><', { desc = 'Decrease window width' })
+vim.keymap.set('n', '<C-Right>', '<C-w>>', { desc = 'Increase window width' })
+
+-- Move lines
+vim.keymap.set('n', '<A-j>', ':m .+1<CR>==', { desc = 'Move line down' })
+vim.keymap.set('n', '<A-k>', ':m .-2<CR>==', { desc = 'Move line up' })
+vim.keymap.set('v', '<A-j>', ":m '>+1<CR>gv=gv", { desc = 'Move selection down' })
+vim.keymap.set('v', '<A-k>', ":m '<-2<CR>gv=gv", { desc = 'Move selection up' })
+
+-- Indent lines
+vim.keymap.set('v', '<', '<gv', { desc = 'Indent left and reselect' })
+vim.keymap.set('v', '>', '>gv', { desc = 'Indent right and reselect' })
+
+-- Join lines
+vim.keymap.set('n', 'J', 'mzJ`z', { desc = 'Join lines and keep cursor position' })
+
+-- Save file
+vim.keymap.set({ 'n' }, '<D-s>', ':w<CR>', { desc = 'Save file' })
+vim.keymap.set({ 'i' }, '<D-s>', '<ESC>:w<CR>a', { desc = 'Save file' })
+
+-- Create new tab
+vim.keymap.set('n', '<C-n>', ':tabe<CR>', { desc = 'Create new tab' })
+
+-- Make terminal splits
+vim.api.nvim_set_keymap('n', '<leader>mtv', ':vsplit | terminal<CR>', { noremap = true, silent = true, desc = '[M]ake [T]erminal ([V]ertical Split)' })
+vim.api.nvim_set_keymap('n', '<leader>mth', ':split | terminal<CR>', { noremap = true, silent = true, desc = '[M]ake [T]erminal ([H]orizontal Split)' })
+-- Make splits
+vim.api.nvim_set_keymap('n', '<leader>msv', ':vsplit<CR>', { noremap = true, silent = true, desc = '[M]ake [S]plit [V]ertical' })
+vim.api.nvim_set_keymap('n', '<leader>msh', ':split<CR>', { noremap = true, silent = true, desc = '[M]ake [S]plit [H]orizontal' })
+-- Make tabs
+vim.api.nvim_set_keymap('n', '<leader>mn', ':tabnew<CR>:Oil<CR>', { noremap = true, silent = true, desc = '[M]ake [N]ew Tab' })
+
+-- Insert new line without entering insert mode and remain on current line
+-- Supports count to insert multiple lines
+vim.keymap.set('n', '<CR>', function()
+  local count = vim.v.count1
+  local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+  local cur_line = vim.api.nvim_buf_get_lines(0, row - 1, row, false)[1] or ''
+  local indent = cur_line:match '^%s*' or ''
+  local lines = {}
+  for i = 1, count do
+    lines[i] = indent
+  end
+  vim.api.nvim_buf_set_lines(0, row, row, true, lines)
+  vim.api.nvim_win_set_cursor(0, { row, col })
+end, { remap = false, silent = true, desc = 'Insert new line(s) below without entering insert mode' })
+vim.keymap.set('n', '<S-CR>', function()
+  local count = vim.v.count1
+  local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+  local cur_line = vim.api.nvim_buf_get_lines(0, row - 1, row, false)[1] or ''
+  local indent = cur_line:match '^%s*' or ''
+  local lines = {}
+  for i = 1, count do
+    lines[i] = indent
+  end
+  local start = row - 1
+  vim.api.nvim_buf_set_lines(0, start, start, true, lines)
+  vim.api.nvim_win_set_cursor(0, { row + count, col })
+end, { remap = false, silent = true, desc = 'Insert new line(s) above without entering insert mode' })
