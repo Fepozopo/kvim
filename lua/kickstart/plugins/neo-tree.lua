cce5818764ef9aa829ec730ec43cffecc6adbd47
Shane DuPrey <36708583+Fepozopo@users.noreply.github.com>
2026-05-31 16:14:57 -0700

    fix(sidekick): adjust CLI window layout
    Move the Sidekick CLI to the left and keep Zellij mux integration
    disabled via the top-level mux config.
    ---
    fix(neo-tree): move tree window to the right
    
    Open Neo-tree on the right and show gitignored files in the
    filesystem view.

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -4,33 +4,34 @@
 ---@module 'lazy'
 ---@type LazySpec
 return {
-    'nvim-neo-tree/neo-tree.nvim',
-    version = '*',
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-        'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-        'MunifTanjim/nui.nvim',
-    },
-    lazy = false,
-    keys = {
-        { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
-    },
-    ---@module 'neo-tree'
-    ---@type neotree.Config
-    opts = {
-        filesystem = {
-            hijack_netrw_behavior = "disabled", -- ensure it doesn't take directory buffers
-            window = {
-                mappings = {
-                    ['\\'] = 'close_window',
-                },
-            },
-            filtered_items = {
-                visible = true,         -- Set to true to show hidden files
-                hide_dotfiles = false,  -- Show dotfiles
-                hide_gitignored = true, -- Show gitignored files
-            },
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  lazy = false,
+  keys = {
+    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+  },
+  ---@module 'neo-tree'
+  ---@type neotree.Config
+  opts = {
+    filesystem = {
+      hijack_netrw_behavior = 'disabled', -- ensure it doesn't take directory buffers
+      window = {
+        mappings = {
+          ['\\'] = 'close_window',
         },
-        open_at_startup = false,
+        position = 'right',
+      },
+      filtered_items = {
+        visible = true, -- Set to true to show hidden files
+        hide_dotfiles = false, -- Show dotfiles
+        hide_gitignored = false, -- Show gitignored files
+      },
     },
+    open_at_startup = false,
+  },
 }
